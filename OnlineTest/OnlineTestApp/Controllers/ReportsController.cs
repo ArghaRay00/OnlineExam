784122f22ab1684ae9ec0b8fabadf7a7ea15e607
ArghaRay00 <reachout_argha@outlook.com>
2016-05-06 02:28:03 +0530

    StudentReport

--- OnlineTest/OnlineTestApp/Controllers/ReportsController.cs
+++ OnlineTest/OnlineTestApp/Controllers/ReportsController.cs
@@ -13,5 +13,10 @@ namespace OnlineTestApp.Controllers
         {
             return View();
         }
+        public ActionResult StudentsEnrolled()
+        {
+            return View();
+        }
+
     }
 }
---
+++ OnlineTest/OnlineTestApp/Models/ReportSelectCollege.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineTestApp.Models
+{
+    public class ReportSelectCollege
+    {
+        public int CollegeId { get; set; }
+        public string CollegeName { get; set; }
+    }
+}
