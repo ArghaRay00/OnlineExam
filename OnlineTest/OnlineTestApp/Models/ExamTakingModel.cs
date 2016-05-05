f4e14fbb57423107651681efcb338d143d0a4b51
ArghaRay00 <reachout_argha@outlook.com>
2016-05-05 10:56:51 +0530

    testing 2

---
+++ OnlineTest/OnlineTestApp/Models/ExamTakingModel.cs
@@ -0,0 +1,14 @@
+﻿using OnlineTestEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineTestApp.Models
+{
+    public class ExamTakingModel
+    {
+        public Question Question { get; set; }
+        public int Choice { get; set; }
+    }
+}
